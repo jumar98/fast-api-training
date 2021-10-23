61e9e622972dddd1530a73d9140759e2a671534a
Julian David Martinez Villarreal <jm.zina@nokia.com>
2021-10-23 15:18:49 -0500

    Validation for query and path parameters

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import  Optional
 from pydantic import BaseModel
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query, Path
 
 app = FastAPI()
 
@@ -24,3 +24,26 @@ def home():
 @app.post('/person/new')
 def create_person(person: Person = Body(...)):
     return person
+
+# Validate query parameters
+
+@app.get('/person/detail')
+def person_detail(
+    name: Optional[str] = Query(
+        default=None, min_length=1, max_length=50, 
+        title='Person Nam',
+        description="This is a person name."
+        ),
+    age: int = Query(..., title="Person Age", description="This a person age.")
+    ):
+    return {name: age}
+
+# Validate path parameter
+
+@app.get('/person/detail/{person_id}')
+def person_detail(
+    person_id: int = Path(..., gt=0, 
+        title="Person ID", 
+        description="This is the person ID."
+    )):
+    return {person_id: "Success"}
